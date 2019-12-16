83fc6ec5b3b74827c5f7f4c4829a18f0c64d24f2
connorjcantrell <connorcantrell@outlook.com>
2019-12-16 15:20:57 -0800

    Added integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,22 +1,35 @@
 use hello::thread_pool::ThreadPool;
 use hello::server;
 use std::net::TcpListener;
-// use reqwest;
+use std::thread;
+use reqwest;
+
 
 
 #[test]
+fn simulate() {
+    thread::spawn(move || {
+        graceful_shutdown()
+    });
+
+    thread::spawn(move || {
+        let client = reqwest::Client::new();
+        client.get("127.0.0.1:7878");
+        client.get("127.0.0.1:7878");
+    });
+}
+
 fn graceful_shutdown() {
-    // use reqwest hello to simulate a client
+    // Simulate client with reqwest
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(4) {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             server::handle_connection(stream);
         });
     }
-
     println!("Shutting down.");
 }
